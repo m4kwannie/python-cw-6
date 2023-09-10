24326fa0c1fd42c249d11638e3f048b03dcda63f
♡ <mai.theyab06@gmail.com>
2023-09-11 00:38:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 # write your code here
+person = {
+    "name": "mai",
+    "age": 16,
+    "hobbies": ["drawing", "cooking", "complaining"],
+    }
+print(person["name"])
+print(person["age"])
+person["age"] = 30
+person["country"] = "spain"
+print(person)
+print(f"number of objects in dictionary: {len((person))}")
+person["hobbies"].append("coding")
+def check_hobbies(person):
+    if len(person["hobbies"]) >= 3:
+        return print("WOW YOU ARE AMAZING!")
+    else:
+        return print("keep exploring your hobbies!")
+check_hobbies(person)
